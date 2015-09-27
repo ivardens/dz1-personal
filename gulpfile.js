64290093d87347851eb77e886aee7367f517806f
varden <vardenjob@gmail.com>
2015-09-26 23:54:37 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// var gulp = require('gulp');
+
+// gulp.task('default', function() {
+//   // place code for your default task here
+// });
+
+
+
+var gulp = require('gulp'),
+	browserSync = require('browser-sync');
+
+gulp.task('server', function (){
+	browserSync({
+		port: 9000,
+		server: {
+			baseDir: 'app'
+		}
+	});
+});
+
+gulp.task('watch', function (){
+	gulp.watch([
+		'app/*.html',
+		'app/js/**/*.js',
+		'app/css/**/*.css'
+	]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'watch']);
