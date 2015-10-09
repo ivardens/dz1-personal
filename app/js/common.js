a4991559905522a6042866b5c21db42d6100e4a0
varden <vardenjob@gmail.com>
2015-10-08 22:02:40 -0700

    fix popup window2

--- app/js/common.js
+++ app/js/common.js
@@ -1,37 +1,54 @@
 // Input file form
-  $(document).ready(function() {
-    $('#open_popup_button').click(openPopup)
+$(document).ready(function () {
+  $('#open_popup_button').click(openPopup);
 
-    $('.input__file').on('change', function() { // по событию change инпут файла
+  $('.input__file').on('change', function () { // по событию change инпут файла
 
-      var
-        $this = $(this),
-        val = $this.val(); // берем value у инпут файла (путь загруженого файла)
+    var
+      $this = $(this),
+      val = $this.val(); // берем value у инпут файла (путь загруженого файла)
 
-      //ч то бы отрезать /fakepath/ (это добавляет сам браузер)
+    //что бы отрезать /fakepath/ (это добавляет сам браузер)
 
-      var
-        regexp = /c:\\fakepath\\/gmi, //регулярное выражение для поиска
-        pureVal = val.replace(regexp, ''); // заменяем то что нашли в строке - на пустоту
+    var
+      regexp = /c:\\fakepath\\/gmi, //регулярное выражение для поиска
+      pureVal = val.replace(regexp, ''); // заменяем то что нашли в строке - на пустоту
 
-      $('.input__fake-text').text(pureVal); // вставляем в блок с текстом имя файла
+    $('.input__fake-text').text(pureVal); // вставляем в блок с текстом имя файла
 
-    })
-  })
+  });
+
+
+});
 // open end close popup window
-  function openPopup() {
-    var container = $('<div class="popup_container"></div>'),
-      overlay = $('<div class="popup_overlay"></div>')
+function openPopup() {
+  var container = $('<div class="popup_container"></div>'),
+    overlay = $('<div class="popup_overlay"></div>')
 
-    $('#popup_template').clone().appendTo(container);
+  $('#popup_template').clone().appendTo(container);
 
-    $(document.body).append(overlay);
-    $(document.body).append(container);
+  $(document.body).append(overlay);
+  $(document.body).append(container);
 
-    container.find('div.popup_add_title > a').one('click', closePopup);
-  }
+  container.find('div.popup_add_title > a').one('click', closePopup);
+}
 
-  function closePopup() {
-    $(document.body).find('.popup_container').remove()
-    $(document.body).find('.popup_overlay').remove()
-  }
+function closePopup() {
+  $(document.body).find('.popup_container').remove();
+  $(document.body).find('.popup_overlay').remove();
+}
+
+$(document).ready(function(){
+
+	$('.form_input-file-origin').on('change', function(){
+
+		var
+			$this = $(this),
+			value = $this.val(),
+			pureVal = value.replace(/c:\\fakepath\\/gmi, "");
+
+		$('.form_input-fake').text(pureVal);
+
+	});
+
+});
